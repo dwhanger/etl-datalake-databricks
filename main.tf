bbf969df92a2df37361a87c5857eebb6ddf212c8
Darren Whanger <dwhanger@microsoft.com>
2023-04-05 22:29:49 -0700

    hardcoded secrets work?

--- main.tf
+++ main.tf
@@ -18,7 +18,6 @@ terraform {
   }
 
   # Update this block with the location of your terraform state file
-/*
   backend "azurerm" {
     resource_group_name  = "terraform-sc-rg"
     storage_account_name = "tysdscinfratfsa"
@@ -26,13 +25,12 @@ terraform {
     key                  = "devintus-stack"
     use_oidc             = true
 ###
-    access_key           = ""
-    subscription_id      = ""
-    client_id            = ""
-    client_secret        = ""
-    tenant_id            = ""
+    access_key            = "z05NUiT6goa2mIrSUrs7u9iIe9KE+D4sDvxIjPhhOHrrfpFKyDRU1T7g1XDwlv1x8fXL7ChIlOOA+AStOgdFhg=="
+    subscription_id       = "dfd44adf-0b1d-4309-b742-988a91722fe7"
+    client_id             = "fe5a7e5d-c3e9-49a7-a9cf-aef07d968c30"
+    client_secret         = "sHw8Q~qDrutv_7ZT.~BxNoSauALTk~e4WLJd0boK"
+    tenant_id             = "4beecb38-fbf2-4aa5-aa0a-4aec609a959a"
   }
-  */
 }
 
 
