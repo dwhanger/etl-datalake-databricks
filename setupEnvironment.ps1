f3885ace249c8023b77e7cdde51cff5bd3f25962
Darren Whanger <dwhanger@microsoft.com>
2023-04-07 07:59:09 -0700

    playing25

--- setupEnvironment.ps1
+++ setupEnvironment.ps1
@@ -1,6 +1,1 @@
 # PowerShell
-$env:ARM_CLIENT_ID = "fe5a7e5d-c3e9-49a7-a9cf-aef07d968c30"
-$env:ARM_CLIENT_SECRET = "sHw8Q~qDrutv_7ZT.~BxNoSauALTk~e4WLJd0boK"
-$env:ARM_TENANT_ID = "4beecb38-fbf2-4aa5-aa0a-4aec609a959a"
-$env:ARM_SUBSCRIPTION_ID = "dfd44adf-0b1d-4309-b742-988a91722fe7"
-$env:ARM_ACCESS_KEY = "z05NUiT6goa2mIrSUrs7u9iIe9KE+D4sDvxIjPhhOHrrfpFKyDRU1T7g1XDwlv1x8fXL7ChIlOOA+AStOgdFhg=="
